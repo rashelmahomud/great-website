1ebec2dd0ce6607fa820928edbcc2fd83dfa4249
rashelmahomud <rashelmahmudraj1998@gmail.com>
2022-04-25 01:56:31 +0600

    added order form here

--- src/Pages/ServiceDetails/CheckOut/CheckOut.js
+++ src/Pages/ServiceDetails/CheckOut/CheckOut.js
@@ -1,14 +1,41 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import useServiceDetails from '../../../hooks/useServiceDetails';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
 
 const CheckOut = () => {
+    const [user] = useAuthState(auth);
     const { servicesId } = useParams();
     const [service] = useServiceDetails(servicesId);
-    
+
+    const handelOrder = event => {
+        event.preventDefault();
+        const order = {
+            email: user.email,
+            service: service.name,
+            servicesId:servicesId,
+            address: event.target.address.value,
+            phone: event.target.phone.value
+        }
+    }
+
     return (
-        <div>
+        <div className='w-50 mx-auto'>
             <h2>Pleases Order: {service.name}</h2>
+            <form onSubmit={handelOrder}>
+                <input className='w-100 mb-3' value={user.displayName} type="text" name='name' placeholder='Enter Your Name-' required disabled />
+                <br />
+                <input className='w-100 mb-3' value={user.email} type="email" name='email' placeholder='Enter Your Email-' required disabled />
+                <br />
+                <input className='w-100 mb-3' value={service.name} type="text" name='service' placeholder='Enter Your Service-' required disabled />
+                <br />
+                <input className='w-100 mb-3' type="text" name='address' placeholder='Enter Your Address-' required />
+                <br />
+                <input className='w-100 mb-3' type="number" name='phone' placeholder='Enter Your Phone-' required />
+                <br />
+                <input className='fw-bold bg-primary text-white rounded-pill' type="submit" value="Order Now" />
+            </form>
         </div>
     );
 };
