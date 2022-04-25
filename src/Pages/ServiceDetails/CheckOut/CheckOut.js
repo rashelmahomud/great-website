180d565317bc354da09aa89785ce88e2fa1f393d
rashelmahomud <rashelmahmudraj1998@gmail.com>
2022-04-25 12:59:58 +0600

    added order page and setup

--- src/Pages/ServiceDetails/CheckOut/CheckOut.js
+++ src/Pages/ServiceDetails/CheckOut/CheckOut.js
@@ -3,6 +3,7 @@ import { useParams } from 'react-router-dom';
 import useServiceDetails from '../../../hooks/useServiceDetails';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import auth from '../../../firebase.init';
+import axios from 'axios';
 
 const CheckOut = () => {
     const [user] = useAuthState(auth);
@@ -18,15 +19,26 @@ const CheckOut = () => {
             address: event.target.address.value,
             phone: event.target.phone.value
         }
+
+        axios.post('http://localhost:5000/order', order)
+        .then(response => {
+            const {data} = response;
+            if(data.insertedId){
+                alert('you are sessful!!')
+                event.target.reset();
+            }
+        })
+
+        
     }
 
     return (
         <div className='w-50 mx-auto'>
             <h2>Pleases Order: {service.name}</h2>
             <form onSubmit={handelOrder}>
-                <input className='w-100 mb-3' value={user.displayName} type="text" name='name' placeholder='Enter Your Name-' required disabled />
+                <input className='w-100 mb-3' value={user?.displayName} type="text" name='name' placeholder='Enter Your Name-' required disabled />
                 <br />
-                <input className='w-100 mb-3' value={user.email} type="email" name='email' placeholder='Enter Your Email-' required disabled />
+                <input className='w-100 mb-3' value={user?.email} type="email" name='email' placeholder='Enter Your Email-' required disabled />
                 <br />
                 <input className='w-100 mb-3' value={service.name} type="text" name='service' placeholder='Enter Your Service-' required disabled />
                 <br />
